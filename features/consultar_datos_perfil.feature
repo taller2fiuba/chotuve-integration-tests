5ab30483df9d2c1d435190051b2da28d93024904
Matias <matiaseiglesias@yahoo.com>
2020-06-08 20:10:09 -0300

    agrego esecenario ver perfil de otro usuario modificado

--- features/consultar_datos_perfil.feature
+++ features/consultar_datos_perfil.feature
@@ -16,3 +16,9 @@ Feature: Consultar datos de perfiles
 		And Complete los campos de mi perfil con nombre “test”, apellido “test”
 		When veo los datos de mi perfil
 		Then veo mi nombre “test”, apellido “test” y email “test@test.com”	
+
+	Scenario: Consultar perfil de otro usuario
+		Given el usuario con email “otrousuario@otro.com” modifico su perfil con nombre “test”, apellido “test”, teléfono “123456789”, dirección “calle falsa 123”
+		And inicie sesión correctamente
+		When veo los datos del perfil del usuario con email otrousuario@otro.com
+		Then veo su nombre “test”, apellido “test”, teléfono “123456789”, dirección “calle falsa 123” y email “otrousuario@otro.com”
--- features/steps/consultar_datos_perfil.py
+++ features/steps/consultar_datos_perfil.py
@@ -53,3 +53,37 @@ def step_impl(context, nombre, apellido, mail):
 	assert context.response.json()["nombre"] == nombre
 	assert context.response.json()["apellido"] == apellido
 	assert context.response.json()["email"] == mail
+
+@Given('el usuario con email “{mail}” modifico su perfil con nombre “{nombre}”, apellido “{apellido}”, teléfono “{telefono}”, dirección “{direccion}”')
+def step_impl(context, mail, nombre, apellido, telefono, direccion):
+	context.response = ChotuveAppServerApiClient().registrarse(mail, "PASSWORD")
+	verificar_codigo_de_respuesta(context, 201)
+	response = ChotuveAppServerApiClient().iniciar_sesion(mail, "PASSWORD")
+	verificar_codigo_de_respuesta(context, 201)
+	context.response = response
+	context.token = response.json().get('auth_token', None)
+	response = ChotuveAppServerApiClient().get_con_token("/usuario/perfil", context)
+	verificar_codigo_de_respuesta(context, 200)
+	context.id_otro_usuario = response.json().get('id', None)
+	datos = {
+		"nombre": nombre,
+		"apellido": apellido,
+		"telefono": telefono,
+		"direccion": direccion
+	}
+	context.response = ChotuveAppServerApiClient().put_con_token("/usuario/perfil", datos, context)
+	verificar_codigo_de_respuesta(context, 200)
+
+	@When('veo los datos del perfil del usuario con email otrousuario@otro.com')
+	def step_impl(context):
+		otro_usuario_id = str(context.id_otro_usuario)
+		context.response = ChotuveAppServerApiClient().get_con_token("/usuario/perfil/"+otro_usuario_id, context)
+		verificar_codigo_de_respuesta(context, 200)
+
+@Then('veo su nombre “{nombre}”, apellido “{apellido}”, teléfono “{telefono}”, dirección “{direccion}” y email “{mail}”')
+def step_impl(context, nombre, apellido, telefono, direccion, mail):
+	assert context.response.json()["nombre"] == nombre
+	assert context.response.json()["apellido"] == apellido
+	assert context.response.json()["telefono"] == telefono
+	assert context.response.json()["direccion"] == direccion
+	assert context.response.json()["email"] == mail
