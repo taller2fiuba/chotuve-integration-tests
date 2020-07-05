14f44ffe255489a9e47a0552b834da74f61a0513
Franco Liberali <franco.liberali@gmail.com>
2020-07-05 20:16:54 -0300

    chequear se mi usuario

--- features/steps/registro_steps.py
+++ features/steps/registro_steps.py
@@ -19,6 +19,8 @@ def step_impl(context):
 @then('me registro exitosamente')
 def step_impl(context):
     assert_status_code(201, context.yo.last_response.status_code)
+    assert context.yo.auth_token != ''
+    assert context.yo.id != ''
 
 @then('veo error de registro porque el mail ya está en uso')
 def step_impl(context):
